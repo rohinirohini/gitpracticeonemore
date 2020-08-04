1568a8928435c45149e9959f8bdfdaf375b5735e
Rohini <gosavirohini1993@gmail.com>
2020-08-04 18:32:36 +0530

    removed comments

--- first/src/first/Aaa.java
+++ first/src/first/Aaa.java
@@ -1,25 +1,13 @@
-/**
- * 
- */
+
 package first;
 
-/**
- * @author rohini
- *
- */
 public class Aaa {
 
-	/**
-	 * 
-	 */
 	public Aaa() {
 		// TODO Auto-generated constructor stub
 		System.out.println("inside constructor Aaa");
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
