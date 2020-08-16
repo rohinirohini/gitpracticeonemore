b8c0d5ca48c9d2ac4826513b0f0f1ab8ef901b17
Rohini <gosavirohini1993@gmail.com>
2020-08-16 18:04:19 +0530

    zz

--- first/src/first/Aaa.java
+++ first/src/first/Aaa.java
@@ -4,34 +4,7 @@ package first;
 public class Aaa {
 
 	public Aaa() {
-		// TODO Auto-generated constructor stub
-		System.out.println("inside constructor Aaa");
-		System.out.println("for chaking commit difference with diff command ");
-		System.out.println("for chaking commit difference with diff command ");
-		
-		System.out.println("for branch in local to merge in master");
-	
-	 System.out.println("for merge from branch3 to master");
-	 
-	 System.out.println(" local branch 3 for commit");
-	 
-	 System.out.println("for rebase ");
-	 
-	 System.out.println("for rebase ........");
-	 System.out.println("nnnnnnnnnnnnnnnnnnn");
-	 System.out.println("brach creation in github and checkout in local ,and then commit in github");
-
-	 
-
-
-	 System.out.println("brach creation in github and checkout in local ,and then commit in github");
-
-	 
-	
-	
-	 
-	
-	}
+                     }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
