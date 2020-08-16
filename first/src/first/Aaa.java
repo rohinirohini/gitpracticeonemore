2ab1311664177f7e01e15515b4ce06b49755d4b7
Rohini <gosavirohini1993@gmail.com>
2020-08-16 18:21:43 +0530

    aaaa

--- first/src/first/Aaa.java
+++ first/src/first/Aaa.java
@@ -5,26 +5,15 @@ public class Aaa {
 
 	public Aaa() {
 		// TODO Auto-generated constructor stub
-		System.out.println("inside constructor Aaa");
-		System.out.println("for chaking commit difference with diff command ");
-		System.out.println("for chaking commit difference with diff command ");
-		
-		System.out.println("for branch in local to merge in master");
 	
-	 System.out.println("for merge from branch3 to master");
-	 
-	 System.out.println(" local branch 3 for commit");
-	 System.out.println("brach creation in github and checkout in local ,and then commit in github");
 
-	 
-	 System.out.println("************");
-	 System.out.println("************");
 
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("branch commit for merge");
 	}
 
 }
